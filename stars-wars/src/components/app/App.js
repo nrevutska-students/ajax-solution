d2221c9e68dad91a9e44f7828664a2d6f6d9eb46
Vladyslav <seile6558@gmail.com>
2021-07-13 20:20:35 +0300

    Added component ItemDescription

--- stars-wars/src/components/app/App.js
+++ stars-wars/src/components/app/App.js
@@ -1,12 +1,13 @@
 import Header from "../header/Header.js"
 import Next from "../nextButton/Next.js"
+import ItemDescription from "../description/ItemDescription.js";
 
 function App() {
   return (
     <div className="app">
      <Header/>
      <Next/>
-      
+     <ItemDescription/>
     </div>
   );
 }
---
+++ stars-wars/src/components/description/ItemDescription.js
@@ -0,0 +1,13 @@
+import "./itemDescription.css"
+
+export default function ItemDescription(props) {//{imgUrl,name,gender,description}
+    return (
+        <div className="itemDescription">
+            <img className="itemImage" src={props.imgUrl}/>
+            <h3>{props.name}</h3>
+            <li className="descItem">Gender:{" "+props.gender}</li>
+            <li className="descItem">Birth:{" "+props.birth_year}</li>
+            <li className="descItem">Eye:{" "+props.eye_color}</li>
+        </div>
+    )
+}
